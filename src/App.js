84337d890f3f2d63789e31d1af0030500f1967fb
michael-duke <michael.21duke@gmail.com>
2022-09-05 13:24:09 +0300

    Incorporate Tailwind into React, and test functionalities

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import 'tw-elements';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p className='bg-red-500'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
+        <button
+          type='button'
+          data-mdb-ripple='true'
+          data-mdb-ripple-color='light'
+          class='inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out'
+        >
+          Math Magician
+        </button>
       </header>
     </div>
   );
