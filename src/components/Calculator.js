9e4adc69c5ada30cbc7b2d8073d383edc2968d03
michael-duke <michael.21duke@gmail.com>
2022-09-07 14:47:15 +0300

    Refactor Calculator.js to be a functional component and use React hook useState

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,219 +1,207 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 import '../styles/Calculator.css';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
+function Calculator() {
+  const [state, setState] = useState({ total: 0, next: null, operation: null });
 
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick = (event) => {
+  const handleClick = (event) => {
     const {
       target: { textContent: buttonName },
     } = event;
-    const result = calculate(this.state, buttonName);
-    this.setState(result);
+    const result = calculate(state, buttonName);
+    setState(result);
   };
 
-  render() {
-    const { total, next } = this.state;
-    return (
-      <div>
-        <section className="Calculator border border-gray-500">
-          <div className="grid grid-rows-6 grid-cols-4 gap-2">
-            {/* Result Display */}
-            <div className="col-span-4 bg-gray-400 text-white text-zl flex items-center justify-end m-[-4px]">
-              {total || next || 0}
-            </div>
-            {/* First row */}
-            <button
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-              type="button"
-              onClick={this.handleClick}
-              className="bg-slate-200"
-            >
-              AC
-            </button>
-            <button
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-              type="button"
-              onClick={this.handleClick}
-              className="bg-slate-200"
-            >
-              +/-
-            </button>
-            <button
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-              type="button"
-              onClick={this.handleClick}
-              className="bg-slate-200"
-            >
-              %
-            </button>
-            <button
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-              type="button"
-              onClick={this.handleClick}
-              className="bg-magic-blue/80 text-white"
-            >
-              &#xf7;
-            </button>
-            {/* Second row */}
-            <button
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-              type="button"
-              onClick={this.handleClick}
-              className="bg-slate-200"
-            >
-              7
-            </button>
-            <button
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-              type="button"
-              onClick={this.handleClick}
-              className="bg-slate-200"
-            >
-              8
-            </button>
-            <button
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-              type="button"
-              onClick={this.handleClick}
-              className="bg-slate-200"
-            >
-              9
-            </button>
-            <button
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-              type="button"
-              onClick={this.handleClick}
-              className="bg-magic-blue/80 text-white"
-            >
-              *
-            </button>
-            {/* Third row */}
-            <button
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-              type="button"
-              onClick={this.handleClick}
-              className="bg-slate-200"
-            >
-              4
-            </button>
-            <button
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-              type="button"
-              onClick={this.handleClick}
-              className="bg-slate-200"
-            >
-              5
-            </button>
-            <button
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-              type="button"
-              onClick={this.handleClick}
-              className="bg-slate-200"
-            >
-              6
-            </button>
-            <button
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-              type="button"
-              onClick={this.handleClick}
-              className="bg-magic-blue/80 text-white"
-            >
-              -
-            </button>
-            {/* Forth row */}
-            <button
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-              type="button"
-              onClick={this.handleClick}
-              className="bg-slate-200"
-            >
-              1
-            </button>
-            <button
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-              type="button"
-              onClick={this.handleClick}
-              className="bg-slate-200"
-            >
-              2
-            </button>
-            <button
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-              type="button"
-              onClick={this.handleClick}
-              className="bg-slate-200"
-            >
-              3
-            </button>
-            <button
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-              type="button"
-              onClick={this.handleClick}
-              className="bg-magic-blue/80 text-white"
-            >
-              +
-            </button>
-            {/* Fifth row */}
-            <button
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-              type="button"
-              onClick={this.handleClick}
-              className="col-span-2 bg-slate-200"
-            >
-              0
-            </button>
-            <button
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-              type="button"
-              onClick={this.handleClick}
-              className="bg-slate-200"
-            >
-              .
-            </button>
-            <button
-              data-mdb-ripple="true"
-              data-mdb-ripple-color="light"
-              type="button"
-              onClick={this.handleClick}
-              className="bg-magic-blue/80 text-white"
-            >
-              =
-            </button>
+  const { total, next } = state;
+  return (
+    <div>
+      <section className="Calculator border border-gray-500">
+        <div className="grid grid-rows-6 grid-cols-4 gap-2">
+          {/*  Result Display  */}
+          <div className="col-span-4 bg-gray-400 text-white text-zl flex items-center justify-end m-[-4px]">
+            {total || next || 0}
           </div>
-        </section>
-      </div>
-    );
-  }
+          {/* First row */}
+          <button
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+            type="button"
+            onClick={handleClick}
+            className="bg-slate-200"
+          >
+            AC
+          </button>
+          <button
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+            type="button"
+            onClick={handleClick}
+            className="bg-slate-200"
+          >
+            +/-
+          </button>
+          <button
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+            type="button"
+            onClick={handleClick}
+            className="bg-slate-200"
+          >
+            %
+          </button>
+          <button
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+            type="button"
+            onClick={handleClick}
+            className="bg-magic-blue/80 text-white"
+          >
+            &#xf7;
+          </button>
+          {/* Second row */}
+          <button
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+            type="button"
+            onClick={handleClick}
+            className="bg-slate-200"
+          >
+            7
+          </button>
+          <button
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+            type="button"
+            onClick={handleClick}
+            className="bg-slate-200"
+          >
+            8
+          </button>
+          <button
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+            type="button"
+            onClick={handleClick}
+            className="bg-slate-200"
+          >
+            9
+          </button>
+          <button
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+            type="button"
+            onClick={handleClick}
+            className="bg-magic-blue/80 text-white"
+          >
+            *
+          </button>
+          {/* Third row */}
+          <button
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+            type="button"
+            onClick={handleClick}
+            className="bg-slate-200"
+          >
+            4
+          </button>
+          <button
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+            type="button"
+            onClick={handleClick}
+            className="bg-slate-200"
+          >
+            5
+          </button>
+          <button
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+            type="button"
+            onClick={handleClick}
+            className="bg-slate-200"
+          >
+            6
+          </button>
+          <button
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+            type="button"
+            onClick={handleClick}
+            className="bg-magic-blue/80 text-white"
+          >
+            -
+          </button>
+          {/* Forth row */}
+          <button
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+            type="button"
+            onClick={handleClick}
+            className="bg-slate-200"
+          >
+            1
+          </button>
+          <button
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+            type="button"
+            onClick={handleClick}
+            className="bg-slate-200"
+          >
+            2
+          </button>
+          <button
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+            type="button"
+            onClick={handleClick}
+            className="bg-slate-200"
+          >
+            3
+          </button>
+          <button
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+            type="button"
+            onClick={handleClick}
+            className="bg-magic-blue/80 text-white"
+          >
+            +
+          </button>
+          {/* Fifth row */}
+          <button
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+            type="button"
+            onClick={handleClick}
+            className="col-span-2 bg-slate-200"
+          >
+            0
+          </button>
+          <button
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+            type="button"
+            onClick={handleClick}
+            className="bg-slate-200"
+          >
+            .
+          </button>
+          <button
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+            type="button"
+            onClick={handleClick}
+            className="bg-magic-blue/80 text-white"
+          >
+            =
+          </button>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default Calculator;
