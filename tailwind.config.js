9878bca0ec3490c9db270ec25e26a3f9f7ca61dc
michael-duke <michael.21duke@gmail.com>
2022-09-13 16:52:37 +0300

    Add respective styles of the App.js component - Add supplement configuration for small screens max:500px - Add boxShadow style of the calculator and adjust it via tailwind.config.js - Add Stange font to App

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,21 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Raleway', 'sans-serif'],
+        sans:['Raleway', 'sans-serif'],                    
+        stange: ['Stange', 'sans-serif'],
         nunito: ['nunito', 'sans-serif'],
-        montserrat: ['Montserrat', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif']
       },
       fontSize: {
         'zl':'1.65rem'
       },
       screens: {
         sm: { max: '768px' },
+        sl: { max: '500px' },
         ms: { max: '1212px' },
+      },
+      boxShadow: {
+        'calcShadow': '0 1px 6px 0 rgba(0, 0, 0, 0.38)',
       },
       colors: {
         'magic-blue': '#137dc5',
