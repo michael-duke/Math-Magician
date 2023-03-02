69fe36cdf0658b7c5e1db696be788030f39cb7ea
Michael Mesfin <michael.21duke@gmail.com>
2023-03-02 22:35:06 +0300

    Update desription in Home Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,16 +5,14 @@ const Home = () => (
     <section className="home mt-7 p-12 space-y-10">
       <h1 className="font-semibold text-2xl">Welcome to our page!</h1>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam amet
-        consequuntur possimus, nulla id praesentium porro voluptatum
-        laboriosam rerum ullam ipsa. Iusto alias quas, distinctio iste
-        voluptates assumenda exercitationem obcaecati.
+        &quot;Math Magicians&quot; is a website for all fans of mathematics. 
+        It is a Single Page App (SPA) that allows users to: make simple calculation, and read a random math-related quote.
       </p>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam amet
-        consequuntur possimus, nulla id praesentium porro voluptatum
-        laboriosam rerum ullam ipsa. Iusto alias quas, distinctio iste
-        voluptates assumenda exercitationem obcaecati.
+        consequuntur possimus, nulla id praesentium porro voluptatum laboriosam
+        rerum ullam ipsa. Iusto alias quas, distinctio iste voluptates assumenda
+        exercitationem obcaecati.
       </p>
     </section>
   </>
